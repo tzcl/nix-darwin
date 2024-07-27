191b55c11e17e4743d077553123253250e3aac43
Toby Law <toby.law@rokt.com>
2024-07-28 00:03:14 +1000

    Make kitty more useable

--- modules/home-manager/kitty.nix
+++ modules/home-manager/kitty.nix
@@ -10,7 +10,7 @@
     settings = {
       top_bar_edge = "top";
       tab_bar_style = "slant";
-      enabled_layouts = "tall";
+      enabled_layouts = "splits, stack";
       macos_option_as_alt = "both";
       shell = "zsh -i";
     };
@@ -20,10 +20,26 @@
       "cmd+shift+t" = "new_tab_with_cwd";
 
       # Prompt editing
-      "alt+backspace" = "send_text all x17";
-      "super+backspace" = "send_text all x15";
-      "super+left" = "send_text all x01";
-      "super+right" = "send_text all x05";
+      "alt+backspace" = "send_text all \\x17";
+      "super+backspace" = "send_text all \\x15";
+      "super+left" = "send_text all \\x01";
+      "super+right" = "send_text all \\x05";
+
+      # Window management
+      "alt+s" = "launch --location=vsplit";
+      "alt+v" = "launch --location=hsplit";
+      "alt+enter" = "launch --location=split";
+      "alt+r" = "layout_action rotate";
+
+      "alt+super+left" = "move_window left";
+      "alt+super+right" = "move_window right";
+      "alt+super+up" = "move_window up";
+      "alt+super+down" = "move_window down";
+
+      "shift+left" = "neighboring_window left";
+      "shift+right" = "neighboring_window right";
+      "shift+up" = "neighboring_window up";
+      "shift+down" = "neighboring_window down";
     };
   };
 }
